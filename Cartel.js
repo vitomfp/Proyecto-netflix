914db693186fa95a80d2b761b60577c2be507e68
Mp-keyner <keyneroswaldo0@gmail.com>
2023-08-20 13:52:37 -0500

    20 de agosto del 1:52

---
+++ Cartel.js
@@ -0,0 +1,8 @@
+const miImagen = document.getElementById("miImagen");
+
+if (screen.width > 600) {
+  console.log(screen.width);
+  miImagen.style.width = "7pc";
+  miImagen.src =
+    "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2Fnetflux.png?alt=media&token=76c0c8b4-f3c3-495f-911f-ae13c0370dfc";
+}
