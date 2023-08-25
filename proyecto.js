80b9b77e44572b742ca824b3e3edcc7fa757164e
Mp-keyner <keyneroswaldo0@gmail.com>
2023-08-25 04:08:37 -0500

    25 de agosto 12:07

--- proyecto.js
+++ proyecto.js
@@ -1,11 +1,16 @@
-const video = document.getElementById('video')
-const play = document.getElementById('play')
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const miImagen = document.getElementById("miImagen");
 
- 
- 
- 
-play.addEventListener('click', ()=>{
-    console.log('kei')
-    video.style.opacity = "1";
-    video.play();
-})
+if (screen.width > 600) {
+  console.log(screen.width);
+  miImagen.style.width = "7pc";
+  miImagen.src =
+    "https://firebasestorage.googleapis.com/v0/b/fb-react-e2e7c.appspot.com/o/netflix%20grande.png?alt=media&token=c66d5fc0-5d93-456d-8544-e492b8453899";
+}
+
+play.addEventListener("click", () => {
+  console.log("kei");
+  video.style.opacity = "1";
+  video.play();
+});
