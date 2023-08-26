2581ecae6aeb94a38252e68c4c95c257a227f538
Mp-keyner <keyneroswaldo0@gmail.com>
2023-08-26 09:12:16 -0500

    26 de agosto 9:12

--- Usert.js
+++ Usert.js
@@ -3,3 +3,26 @@ const Userr = document.getElementById("Userr");
 Userr.addEventListener("click", () => {
   console.log("helo");
 });
+
+const inputs = document.querySelectorAll("input[type='number']");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    const inputValue = input.value;
+
+    if (inputValue.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+
+    if (index === inputs.length - 1 && inputValue.length === 1) {
+      input.blur(); // Quitar el enfoque del último campo después de ingresar un carácter
+    }
+  });
+
+  // Controlar el retroceso para cambiar al campo anterior
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
+});
